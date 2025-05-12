f490adb3c6ab76118c4c12dc88a9e3f366a33758
sayfullo2212 <sayfulloturayev5@gmail.com>
2025-05-12 09:35:53 +0500

    7-masala

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,16 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    if len(s1)%2==1 and len(s2)%2==1 and len(s3)%2==1 :
+        return len(s1) and len(s2) and len(s3) 
+    if len(s1)%2==1 and len(s2)%2==1 :
+        return len(s1) and len(s2)
+    if len(s1)%2==1 :
+        return len(s1)
+    if len(s2)%2==1 and len(s3)%2==1 :
+        return len(s2) and len(s3)
+    if len(s2)%2==1 :
+        return len(s2)
+    if len(s3)%2==1 :
+        return len(s3)
+    
